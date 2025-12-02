10e43ba8ed4088fdb17ef00fb4177438a31e7988
gongtaoxiang <gongtaoxiang@creditease.cn>
2025-12-02 20:58:02 +0800

    1.universe-id-generator初始化+demo 2.mask初始化

--- universe-demo/src/main/java/cn/xgt/vo/CustomerEntity.java
+++ universe-demo/src/main/java/cn/xgt/vo/CustomerEntity.java
@@ -19,7 +19,7 @@ public class CustomerEntity {
     /** 证件类别。96.香港身份证,97.澳门身份证,98.台湾身份证,111.居民身份证,414.普通护照,511.台湾居民来往大陆通行证,516.港澳居民来往内地通行证,517.大陆居民往来台湾通行证,714.统一社会信用代码 */
     private String cardType ;
     /** 证件号 */
-    @Mask(category = CATEGORT.ID_NUM)
+    /*@Mask(category = CATEGORT.ID_NUM)*/
     private String cardNo ;
     /** 性别。1.男，2.女 */
     private String sex ;
